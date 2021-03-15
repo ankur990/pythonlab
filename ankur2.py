1e0f8560a0a0238a6488f54cf8aeb02821692dff
ankur990 <80679726+ankur990@users.noreply.github.com>
2021-03-15 15:31:13 +0530

    lower case  or upper case

---
+++ ankur2.py
@@ -0,0 +1,9 @@
+n=input()
+if n>='A' and n<='Z':
+          print("Upper case Alphabet")
+elif n>='a' and n<='z':
+          print("Lower case Alphabet")
+elif n>='0' and n<='9':
+          print("digit")
+else:
+          print("Special character")
