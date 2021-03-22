b5f58712ba41d68bbab7a22629940c3a9bda9da0
ankur990 <80679726+ankur990@users.noreply.github.com>
2021-03-22 15:38:15 +0530

    CHECK ENTERED YEAR IS LEAP YEAR OR NOT

---
+++ ankur12.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the year"))
+if n%400==0:
+	print("Inputted year is a leap year")
+elif n%100==0:
+	print("Inputted year is not a leap year")
+elif n%4==0:
+	print("Inputted year is a leap year")
